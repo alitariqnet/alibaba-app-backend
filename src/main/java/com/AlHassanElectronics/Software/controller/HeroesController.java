7943e9bfd9ed85438324e6a7ca9c9d363e49ba60
alitariqnet <alibabapk@aol.com>
2024-03-24 17:17:12 +0500

    Heroes controller added

---
+++ src/main/java/com/AlHassanElectronics/Software/controller/HeroesController.java
@@ -0,0 +1,10 @@
+package com.AlHassanElectronics.Software.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController(value = "heroes")
+@RequestMapping(name = "api/heroes")
+public class HeroesController {
+    
+}

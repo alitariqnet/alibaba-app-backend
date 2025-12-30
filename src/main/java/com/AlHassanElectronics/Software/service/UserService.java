f9b25c6fc576a0a82aca415309223a2d39ef5da7
Ali Tariq <alibabapk@aol.com>
2025-12-30 13:23:50 +0500

    Remove unused import

--- src/main/java/com/AlHassanElectronics/Software/service/UserService.java
+++ src/main/java/com/AlHassanElectronics/Software/service/UserService.java
@@ -1,6 +1,5 @@
 package com.AlHassanElectronics.Software.service;
 
-import com.AlHassanElectronics.Software.Application;
 import com.AlHassanElectronics.Software.entity.User;
 import com.AlHassanElectronics.Software.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
